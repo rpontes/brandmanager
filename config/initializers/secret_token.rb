5b02d615e40cf9e92fc41372206554d5e91d1ef2
Ricardo Pontes <rpontes@gmail.com>
2012-07-31 20:33:41 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brandmanager::Application.config.secret_token = '1a9bd614379896dbc68eb3adbeb15f1783c235e69f13fb23592de51d8f7ec0b6442a7db53ffa3be9f17902a669abe9ce7570e4d523a8fdf15249988a4167a043'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Brandmanager::Application.config.session_store :cookie_store, key: '_brandmanager_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Brandmanager::Application.config.session_store :active_record_store
